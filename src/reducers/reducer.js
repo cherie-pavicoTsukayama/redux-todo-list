22bd52be6bbd1bd8ceda399c2c6d1f53a78076c4
Cherie <cherietsukayama@gmail.com>
2020-07-15 16:58:27 +0000

    created the reducer files

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -4,36 +4,46 @@ import {
   ADD_TODO,
   TOGGLE_TODO
 } from '../actions';
+import {combineReducers} from 'redux';
 
-//We'll start by specifying the initial state. Redux will call our reducer with
-//an undefined state for the first time. This is our chance to return the initial
-//state of our app:
-const initialState = {
-  visibilityFilter: VisibilityFilteres.SHOW_ALL,
-  todos: []
-}
+const { SHOW_ALL } = VisibilityFilteres;
 
-function todoApp(state = initialState, action) {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return { ...state, visibilityFilter: action.filter };
+function todos(state =[], action) {
+  switch(action.type) {
     case ADD_TODO:
-      return {...state, todos:[
-        ...state.todos,
+      return [
+        ...state,
         {
           text: action.text,
           completed: false
         }
-      ]}
+      ];
     case TOGGLE_TODO:
-      return {...state, todos: state.todos.map((todo, index) => {
+      return state.map((todo, index) => {
         if (index === action.index) {
-          return {...todo, completed: !todo.completed}
-          }
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
         return todo;
-        })
-      }
-    default:
-      return state
+      })
+      default:
+        return state;
   }
 }
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch(action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+const todoApp = combineReducers({
+  visibilityFilter: visibilityFilter,
+  todos: todos
+})
+
+export default todoApp;
