9783df8050c38894751db12f9ffb48698d17da77
Cherie <cherietsukayama@gmail.com>
2020-07-16 19:27:08 +0000

    added styling to the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import FilterLink from '../containers/FilterLink';
 import { VisibilityFilters } from '../actions';
 
 const Footer = () => (
-  <div className="thin-grey-border col-3">
-    <p>
+  <div className="thin-grey-border col-3 d-flex flex-wrap justify-content-center py-3">
+    <h3 className="montserrat-400 text-center">View</h3>
     <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
     <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
     <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
-    </p>
+
   </div>
 
 )
