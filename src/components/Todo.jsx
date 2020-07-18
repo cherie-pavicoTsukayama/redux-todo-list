06171019e55e3403aa500bc5c773804e0716e96d
Cherie <cherietsukayama@gmail.com>
2020-07-18 18:13:29 +0000

    added the edit pencil icon

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,21 +2,29 @@ import React from 'react';
 // import PropTypes from 'prop-types'
 
 const Todo = ({onClick, completed, text}) => {
-  const icon = !completed ? 'far fa-circle light-grey' : 'fas fa-check-circle green'
+  const icon = !completed ? 'far fa-circle light-grey pointer' : 'fas fa-check-circle green pointer'
   return(
       <li
-        className="thin-grey-border ml-4 col-12 p-4 d-flex align-items-center"
+        className="thin-grey-border ml-4 col-12 p-4 "
         onClick={onClick}
 
       >
-        <i className={`${icon} mr-4 fa-2x `}></i>
-        <span
-          style={{
-            textDecoration: completed ? 'line-through' : 'none'
-          }}
-        >
-          {text}
-        </span>
+      <div className="d-flex align-items-center justify-content-between">
+        <div className="d-flex align-items-center col-10 pl-0">
+          <i className={`${icon} mr-4 fa-2x `}></i>
+          <span
+            style={{
+              textDecoration: completed ? 'line-through' : 'none'
+            }}
+          >
+            {text}
+          </span>
+        </div>
+        <div className="col d-flex justify-content-end">
+          <i className="fas fa-pencil-alt light-grey pointer"></i>
+        </div>
+        </div>
+
       </li>
   )
 }
