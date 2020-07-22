98383ddc837e83009e6207007eb1e2ecb49b5328
Cherie <cherietsukayama@gmail.com>
2020-07-22 16:37:23 +0000

    added the onChange to the input field when the edit todo icon is clicked

--- src/components/EditTodo.jsx
+++ src/components/EditTodo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const EditTodo = ({text, id}) => {
+const EditTodo = ({todo, id, editTodo}) => {
+
   return (
     <div>
       <form>
-        <input type="text-area" value={text }/>
+        <input type="text-area" value={todo.text} onChange={(event) => editTodo(id, event.target.value)}/>
         <button>Update</button>
       </form>
     </div>
--- src/containers/EditTodo.jsx
+++ src/containers/EditTodo.jsx
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
 import EditTodo from '../components/EditTodo';
+import { editTodo } from '../actions';
 
 const getSingleTodo = (todos, id) => {
   const todo = todos.filter(todo => todo.id === id);
-  return todo;
+  return todo[0];
 }
 
 
@@ -16,7 +17,7 @@ const mapStateToProps = (state, ownProps) => {
 }
 
 const mapDispatchToPops = dispatch => ({
-
+  editTodo: (id, text) => dispatch(editTodo(id, text))
 })
 
 const HandleChangeEditTodo = connect(mapStateToProps, mapDispatchToPops)(EditTodo)
